5695d2873e544fcb4ad12f0660e89ec1015c1499
Paul Grey <paulgnz@users.noreply.github.com>
2023-07-28 19:11:34 +1200

    Update arbTrade.js
    added protondex_precsion.json

--- arbTrade.js
+++ arbTrade.js
@@ -1,8 +1,15 @@
 require('dotenv').config();
 const ccxt = require('ccxt-xpr');
+const fs = require('fs');
 
-const symbolProtonDEX = 'XPR_XMD';
-const symbolKucoin = 'XPR/USDT';
+let rawdata = fs.readFileSync('protondex_precision.json');
+let markets = JSON.parse(rawdata);
+
+const symbolProtonDEX = 'XDOGE_XMD';
+const symbolKucoin = 'DOGE/USDT';
+
+// const symbolProtonDEX = 'XPR_XMD';
+// const symbolKucoin = 'XPR/USDT';
 
 async function fetchPrices() {
   const exchangeProtonDEX = new ccxt.protondex({
@@ -28,9 +35,23 @@ async function fetchPrices() {
     //console.log('ProtonDEX Ticker:', protondexTicker);
     //console.log('Kucoin Ticker:', kucoinTicker);
 
-    const protondexPrice = protondexTicker.last;
-    const kucoinPrice = kucoinTicker.bid;
+    const marketsKucoin = await exchangeKucoin.fetchMarkets();
 
+    const marketProtonDEX = markets.markets.find(market => market.symbol === symbolProtonDEX);
+    const marketKucoin = marketsKucoin.find(market => market.symbol === symbolKucoin);
+
+    // console.log('marketProtonDEX:', marketProtonDEX);
+    // console.log('marketKucoin:', marketKucoin);
+    
+    const precisionProtonDEX = marketProtonDEX.bid_token.precision;
+    const precisionKucoin = marketKucoin.precision.price;
+
+    const protondexPrice = Number(protondexTicker.last.toFixed(precisionProtonDEX));
+    const kucoinPrice = Number(kucoinTicker.bid);
+
+    // console.log('MarketsProtonDEX:', marketsProtonDEX);
+    // console.log('MarketsKucoin:', marketsKucoin);
+    
     console.log('ProtonDEX Price:', protondexPrice);
     console.log('Kucoin Price:', kucoinPrice);
 
@@ -38,10 +59,10 @@ async function fetchPrices() {
       const spreadPercentage = Math.abs(((kucoinPrice - protondexPrice) / protondexPrice) * 100);
       console.log('Spread Percentage:', spreadPercentage.toFixed(2) + '%');
 
-      const arbitrageThreshold = 0.5;
+      const arbitrageThreshold = 1;
       if (spreadPercentage > arbitrageThreshold) {
         console.log('Arbitrage opportunity detected!');
-        const minOrderValue = 2; // Minimum order value in USDT
+        const minOrderValue = 5; // Minimum order value in USDT
         const maxPrice = Math.max(protondexPrice, kucoinPrice); // Use the higher price
         const amount = minOrderValue / maxPrice; // Amount of XPR to buy or sell        
 
@@ -49,10 +70,8 @@ async function fetchPrices() {
         // If Kucoin price is higher, buy on ProtonDEX and sell on Kucoin
         if (kucoinPrice > protondexPrice) {
             try {
-                console.log('Amount:', Number(amount.toFixed(4)));
-                console.log('Price:', Number(protondexPrice.toFixed(6)));                
-                console.log(`Trying to buy ${amount} XPR on ProtonDEX at price ${protondexPrice} and sell on Kucoin at price ${kucoinPrice}`);
-                const orderProtonDEX = await exchangeProtonDEX.createOrder(symbolProtonDEX, 1, 1, Number(amount.toFixed(4)), Number(protondexPrice.toFixed(6)), {
+                console.log(`Trying to buy ${amount} TOKEN on ProtonDEX at price ${protondexPrice} and sell on Kucoin at price ${kucoinPrice}`);
+                const orderProtonDEX = await exchangeProtonDEX.createOrder(symbolProtonDEX, 1, 1, amount, protondexPrice, {
                     'account': process.env.PROTONDEX_ACCOUNT,
                     'filltype': 0,
                     'triggerprice': 0,
@@ -71,12 +90,9 @@ async function fetchPrices() {
         } 
         // If ProtonDEX price is lower, buy on Kucoin and sell on ProtonDEX
         else {
-            try {
-                console.log('Amount:', Number(amount.toFixed(4)));
-                console.log('Price:', Number(protondexPrice.toFixed(6)));
-                                
-                console.log(`Trying to buy ${amount} XPR on Kucoin at price ${kucoinPrice} and sell on ProtonDEX at price ${protondexPrice}`);
-                const orderProtonDEX = await exchangeProtonDEX.createOrder(symbolProtonDEX, 1, 2, Number(amount.toFixed(4)), Number(protondexPrice.toFixed(6)), {
+            try {                               
+                console.log(`Trying to buy ${amount} TOKEN on Kucoin at price ${kucoinPrice} and sell on ProtonDEX at price ${protondexPrice}`);
+                const orderProtonDEX = await exchangeProtonDEX.createOrder(symbolProtonDEX, 1, 2, amount, protondexPrice, {
                     'account': process.env.PROTONDEX_ACCOUNT,
                     'filltype': 0,
                     'triggerprice': 0,
