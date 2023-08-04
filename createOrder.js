577d068d47f4f44a02e3cce6bb7b893785ceca0f
Paul Grey <paulgnz@users.noreply.github.com>
2023-08-04 16:14:36 +1200

    Update createOrder.js

--- createOrder.js
+++ createOrder.js
@@ -1,32 +1,60 @@
+require('dotenv').config();
 const ccxt = require('ccxt-xpr');
 
+// OPTIONS
+const symbol = 'XPR_XMD'; // Replace with the trading pair symbol you want to trade (remember XBTC not BTC)
+let dollarAmount = 5; // Replace with the amount you want to buy or sell (in XMD / Dollars)
+const price = 0.00092815; // Replace with the price at which you want to place the order
+const orderSide = 1; // Set the order side, 1 for buy and 2 for sell
+
+// FUNCTIONS
 async function placeLimitBuyOrder() {
-  const exchange = new ccxt.protondex({
-    'secret': 'PVT_K1_',
-    'verbose': process.argv.includes('--verbose'),
-    'timeout': 60000,
-  });
-
-  const symbol = 'XPR_XMD'; // Replace with the trading pair symbol you want to trade
-  const amount = 1; // Replace with the amount you want to buy or sell (in XMD / Dollars)
-  const price = 0.000916; // Replace with the price at which you want to place the order
-
-  try {
-    // Load markets and fetch the symbol info
-    await exchange.loadMarkets();
-
-    // Place a limit buy order
-    const order = await exchange.createOrder(symbol, 1, 1, amount, price, {
-      'account': 'trading.paul',
-      'filltype': 0,
-      'triggerprice': 0,
+    const exchange = new ccxt.protondex({
+        'secret': process.env.PROTONDEX_API_SECRET,
+        'verbose': false,
+        'timeout': 60000,
     });
 
-    console.log('Order placed successfully:');
-    console.log(order);
-  } catch (error) {
-    console.error('Error placing the order:', error.message);
-  }
+    // fetch markets from protondex and get precision
+    const markets = await exchange.fetchMarkets();
+    const market = markets.find(market => market.symbol === symbol);
+    console.log('Market:', market);
+
+    // set precision depending on the order side buy or sell
+    let symbolPrecision;
+    if (orderSide === 1) {
+        symbolPrecision = market.info.ask_token.precision;
+    } else if (orderSide === 2) {
+        symbolPrecision = market.info.bid_token.precision;
+    }
+    console.log('SymbolPrecision:', symbolPrecision);
+
+    // calculate the amount of XPR to buy or sell taking into consideration the precision
+    let protondexAmount = dollarAmount;
+    if (orderSide === 1) {
+        protondexAmount = dollarAmount;
+    } else if (orderSide === 2) {
+        protondexAmount = Number((dollarAmount / price).toFixed(symbolPrecision));
+    }
+
+    console.log('Amount of', symbol,'placed:', protondexAmount);
+
+    try {
+    // Load markets and fetch the symbol info
+        await exchange.loadMarkets();
+
+        const order = await exchange.createOrder(symbol, 1, orderSide, dollarAmount, price, {
+            'account': process.env.PROTONDEX_ACCOUNT,
+            'filltype': 0,
+            'triggerprice': 0,
+        });
+
+        console.log('Order placed successfully:');
+        // print the log
+        console.log(order);
+    } catch (error) {
+        console.error('Error placing the order:', error.message);
+    }
 }
 
 placeLimitBuyOrder();
