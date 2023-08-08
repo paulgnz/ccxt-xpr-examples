3f312464de0e8d9c80695add52d2c87ccb2ba208
Paul Grey <paulgnz@users.noreply.github.com>
2023-08-08 17:54:37 +1200

    Update fetchExchanges.js
    support for multiple exchanges

--- src/fetchExchanges.js
+++ src/fetchExchanges.js
@@ -1,21 +1,38 @@
 const ccxt = require('ccxt-xpr');
 
-async function fetchExchanges() {
+async function fetchExchanges(exchangeName) {
     const exchangeProtonDEX = new ccxt.protondex({
         'secret': process.env.PROTONDEX_API_SECRET_2,
         'verbose': false,
         'timeout': 60000,
     });
 
-    const exchangeKucoin = new ccxt.kucoin({
-        'apiKey': process.env.KUCOIN_API_KEY,
-        'secret': process.env.KUCOIN_API_SECRET,
-        'password': process.env.KUCOIN_API_PASSWORD,
-        'verbose': false,
-        'timeout': 60000,
-    });
+    let exchange;
 
-    return { exchangeProtonDEX, exchangeKucoin };
+    switch(exchangeName) {
+        case 'Kucoin':
+            exchange = new ccxt.kucoin({
+                'apiKey': process.env.KUCOIN_API_KEY,
+                'secret': process.env.KUCOIN_API_SECRET,
+                'password': process.env.KUCOIN_API_PASSWORD,
+                'verbose': false,
+                'timeout': 60000,
+            });
+            break;
+        case 'Gateio':
+            exchange = new ccxt.gateio({
+                'apiKey': process.env.GATEIO_API_KEY,
+                'secret': process.env.GATEIO_API_SECRET,
+                'verbose': false,
+                'timeout': 60000,
+            });
+            break;
+        // Add more cases for other exchanges as needed
+        default:
+            console.log(`Exchange ${exchangeName} not recognized`);
+    }
+
+    return { exchangeProtonDEX, exchange };
 };
 
 module.exports = fetchExchanges;
